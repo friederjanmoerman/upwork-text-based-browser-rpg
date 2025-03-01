e619efd69f767d2c657b1d04ce5134b97161fd5d
Hardcodepunk <howl@hardcodepunk.be>
2025-03-01 10:44:15 +0530

    medusa ui implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { TextInput } from "@/components/atoms/TextInput/TextInput"
+import { Button } from "@medusajs/ui"
 
 export default function Home() {
-  return <TextInput placeholder="Username" />
+  return (
+    <>
+      <TextInput placeholder="Username" />
+      <Button>Button</Button>
+    </>
+  )
 }
---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+export default {
+  presets: [import("@medusajs/ui-preset")],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
