12545d8f2fb9614c9f80f773739ea7e6185eb8bd
Hardcodepunk <howl@hardcodepunk.be>
2025-03-04 09:05:28 +0530

    medusa ui fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,22 @@
-import preset from "@medusajs/ui-preset"
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  presets: [preset],
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@medusajs/ui/dist/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+  presets: [require("@medusajs/ui-preset")],
 }
 
 export default config
