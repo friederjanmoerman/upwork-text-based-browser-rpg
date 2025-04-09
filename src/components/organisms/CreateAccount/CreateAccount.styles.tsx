c4ede4dd06c15698da72c49a18bfdc479f5dd7ca
Hardcodepunk <howl@hardcodepunk.be>
2025-04-09 20:38:04 +0530

    start profile bar

--- src/components/organisms/CreateAccount/CreateAccount.styles.tsx
+++ src/components/organisms/CreateAccount/CreateAccount.styles.tsx
@@ -1,76 +1,38 @@
-import { Badge, Container } from "@medusajs/ui"
 import styled from "styled-components"
 
-export const StyledWrapper = styled(Container)`
-  margin: auto;
-  max-width: 888px;
-  height: 100%;
-  max-height: 420px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 0;
-  overflow: hidden;
-`
-
-export const StyledLeftPanel = styled.img`
-  flex: 0 0 50%;
-`
-
-export const StyledRightPanel = styled.div`
-  flex: 0 0 50%;
-  height: 100%;
-`
-
-export const StyledFormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 0.5rem;
-  height: 100%;
-`
-
-export const StyledHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 250px;
-`
-
-export const StyledTitleWithBadge = styled.div`
-  display: flex;
-  gap: 12px;
-`
-
-export const StyledBadge = styled(Badge)`
-  color: #fbbf24;
-  background: #451a03;
-  border: 1px solid #78350f;
-  height: 20px;
-  text-transform: uppercase;
-  padding: 2px 4px;
-`
-
-export const StyledTitle = styled.h1`
-  font-weight: 500;
-  font-size: 13px;
-  line-height: 20px;
-  color: #fafafa;
-`
-
-export const StyledSubtitle = styled.h2`
-  font-weight: 500;
-  font-size: 12px;
-  line-height: 20px;
+export const StyledSidebar = styled.nav`
+  width: 220px;
+  height: 100vh;
+  background-color: #141414;
   color: #71717a;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  font-size: 12px;
 `
 
-export const StyledFormContainerFooter = styled.div`
-  background: #2a2a2a;
-  padding: 12px 16px;
+export const StyledSearchInputWrapper = styled.div`
   width: 100%;
-  align-self: flex-end;
-  border: 1px solid rgba(255, 255, 255, 0.1);
-  margin-top: auto;
+  padding: 12px;
+`
+
+export const StyledNavList = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
   display: flex;
-  justify-content: flex-end;
+  flex-direction: column;
+  gap: 8px;
+  padding: 12px 0;
+  margin: 0 12px;
+  border-top: 1px dotted #71717a;
+`
+
+export const StyledFooter = styled.footer`
+  margin-top: auto;
+  padding: 1rem;
+  font-size: 0.8rem;
+  text-align: center;
+  border-top: 1px solid #2a2a2a;
+  color: #888;
 `
---
+++ src/components/organisms/ProfileBar/ProfileBar.tsx
@@ -0,0 +1,76 @@
+// src/components/organisms/Sidebar/Sidebar.tsx
+
+import React from "react"
+import { StyledSidebar, StyledNavList, StyledFooter, StyledSearchInputWrapper } from "./Sidebar.styles"
+import {
+  AirplaneTilt,
+  Bank,
+  ShoppingCartSimple,
+  PokerChip,
+  Barbell,
+  FirstAid,
+  Newspaper,
+  LockLaminated,
+  HouseLine,
+  Lamp,
+  CrosshairSimple,
+  Fire,
+  Car,
+  Pill,
+  NotePencil,
+} from "@phosphor-icons/react"
+
+import { NavItem } from "@/components/atoms/NavItem/NavItem"
+import { SearchInput } from "@/components/atoms/SearchInput/SearchInput"
+
+interface NavItemData {
+  label: string
+  background?: string
+  icon?: React.ElementType
+}
+
+const navItemsMissions: NavItemData[] = [
+  { label: "Commit A Crime", icon: Fire },
+  { label: "Grand Theft Auto", icon: Car },
+  { label: "Drugs", icon: Pill },
+  { label: "Contract", icon: NotePencil },
+]
+
+const navItemsPlaces: NavItemData[] = [
+  { label: "Airport", icon: AirplaneTilt, background: "#3b82f7" },
+  { label: "Bank", icon: Bank, background: "#6bd45f" },
+  { label: "Black Market", icon: ShoppingCartSimple, background: "#2c2c2e" },
+  { label: "Casino", icon: PokerChip, background: "#f2f2f2" },
+  { label: "Gym", icon: Barbell, background: "#f2a33c" },
+  { label: "Hospital", icon: FirstAid, background: "#eb445a" },
+  { label: "News", icon: Newspaper, background: "#3b82f7" },
+  { label: "Prison", icon: LockLaminated, background: "#2c2c2e" },
+  { label: "Real Estate Agency", icon: HouseLine, background: "#5e5cde" },
+  { label: "Red Light District", icon: Lamp, background: "#eb445a" },
+  { label: "Shooting Range", icon: CrosshairSimple, background: "#2c2c2e" },
+]
+
+const ProfileBar() => {
+  return (
+    <StyledSidebar>
+      <StyledSearchInputWrapper>
+        <SearchInput />
+      </StyledSearchInputWrapper>
+      <StyledNavList>
+        {navItemsMissions.map(item => (
+          <NavItem key={item.label} label={item.label} icon={item.icon} background={item.background} />
+        ))}
+      </StyledNavList>
+      <StyledNavList>
+        The City
+        {navItemsPlaces.map(item => (
+          <NavItem key={item.label} label={item.label} icon={item.icon} background={item.background} />
+        ))}
+      </StyledNavList>
+
+      <StyledFooter>&copy; {new Date().getFullYear()} Crime Club</StyledFooter>
+    </StyledSidebar>
+  )
+}
+
+export default ProfileBar();
