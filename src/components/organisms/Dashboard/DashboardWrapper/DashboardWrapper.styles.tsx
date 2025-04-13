fc6c591abf865dc9481959b8d3df6d8b76a41e10
Hardcodepunk <howl@hardcodepunk.be>
2025-04-13 17:47:10 +0530

    profile card added

--- src/components/organisms/Dashboard/DashboardWrapper/DashboardWrapper.styles.tsx
+++ src/components/organisms/Dashboard/DashboardWrapper/DashboardWrapper.styles.tsx
@@ -4,6 +4,7 @@ export const StyledDashboardWrapper = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
+  background-color: #141414;
 `
 
 export const StyledDashboardContent = styled.div`
