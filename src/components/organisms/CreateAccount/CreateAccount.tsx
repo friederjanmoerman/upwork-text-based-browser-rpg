20a38678e8fb0e92911b8fa50c2a3f69ee33925c
Hardcodepunk <howl@hardcodepunk.be>
2025-04-14 16:45:36 +0530

    fix createAccount imports

--- src/components/organisms/CreateAccount/CreateAccount.tsx
+++ src/components/organisms/CreateAccount/CreateAccount.tsx
@@ -1,8 +1,18 @@
 import { PasswordInput } from "@/components/atoms/PasswordInput/PasswordInput"
 import { TextInput } from "@/components/atoms/TextInput/TextInput"
 import { ButtonWithIcon } from "@/components/atoms/ButtonWithIcon/ButtonWithIcon"
-
-
+import {
+  StyledWrapper,
+  StyledLeftPanel,
+  StyledRightPanel,
+  StyledFormContainer,
+  StyledHeader,
+  StyledTitleWithBadge,
+  StyledTitle,
+  StyledBadge,
+  StyledSubtitle,
+  StyledFormContainerFooter,
+} from "./CreateAccount.styles"
 
 const handleButtonClick = () => {
   // Your logic here
@@ -11,26 +21,26 @@ const handleButtonClick = () => {
 
 export function CreateAccount() {
   return (
-    // <StyledWrapper>
-    //   <StyledLeftPanel src="/illustrations/splash.png" alt="Splash" />
-    //   <StyledRightPanel>
-    //     <StyledFormContainer>
-    //       <StyledHeader>
-    //         <StyledTitleWithBadge>
-    //           <StyledTitle>Crime club</StyledTitle>
-    //           <StyledBadge>Beta</StyledBadge>
-    //         </StyledTitleWithBadge>
-    //         <StyledSubtitle>Create your account</StyledSubtitle>
-    //       </StyledHeader>
-    //       <TextInput placeholder="Username" />
-    //       <TextInput placeholder="Email" />
-    //       <PasswordInput placeholder="Password" />
-    //       <PasswordInput placeholder="Repeat Password" />
-    //       <StyledFormContainerFooter>
-    //         <ButtonWithIcon onClick={handleButtonClick} label="Create account"></ButtonWithIcon>
-    //       </StyledFormContainerFooter>
-    //     </StyledFormContainer>
-    //   </StyledRightPanel>
-    // </StyledWrapper>
+    <StyledWrapper>
+      <StyledLeftPanel src="/illustrations/splash.png" alt="Splash" />
+      <StyledRightPanel>
+        <StyledFormContainer>
+          <StyledHeader>
+            <StyledTitleWithBadge>
+              <StyledTitle>Crime club</StyledTitle>
+              <StyledBadge>Beta</StyledBadge>
+            </StyledTitleWithBadge>
+            <StyledSubtitle>Create your account</StyledSubtitle>
+          </StyledHeader>
+          <TextInput placeholder="Username" />
+          <TextInput placeholder="Email" />
+          <PasswordInput placeholder="Password" />
+          <PasswordInput placeholder="Repeat Password" />
+          <StyledFormContainerFooter>
+            <ButtonWithIcon onClick={handleButtonClick} label="Create account"></ButtonWithIcon>
+          </StyledFormContainerFooter>
+        </StyledFormContainer>
+      </StyledRightPanel>
+    </StyledWrapper>
   )
 }
